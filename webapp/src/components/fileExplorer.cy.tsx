2f5b86700f1ecfacaa8de3c67a3573252734f05c
Case Walker <case.b.walker@gmail.com>
2024-04-14 22:41:45 -0400

    Add icons, put buttons in the FileExplorer

--- webapp/src/components/fileExplorer.cy.tsx
+++ webapp/src/components/fileExplorer.cy.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import FileExplorer from "./fileExplorer";
 
 describe("<FileExplorer />", () => {
-  it("renders", () => {
+  it("has two buttons, one to create a new directory, one to upload a file", () => {
     cy.mount(<FileExplorer />);
+    cy.get("button").should("contain.text", "New Folder");
+    cy.get("button").should("contain.text", "Upload File");
   });
 });
--- webapp/src/components/fileExplorer.tsx
+++ webapp/src/components/fileExplorer.tsx
@@ -1,5 +1,24 @@
+import { Button } from "@mui/material";
+import { CloudUpload, Folder } from "@mui/icons-material";
+
 export default function FileExplorer() {
   return (
-    <h1>Hello world</h1>
+    <div>
+      {/* Todo figure out how to make normal-case */}
+      <Button
+        variant="contained"
+        disableRipple={true}
+        startIcon={<Folder />}
+      >
+        New Folder
+      </Button>
+      <Button
+        variant="contained"
+        disableRipple={true}
+        startIcon={<CloudUpload />}
+      >
+        Upload File
+      </Button>
+    </div>
   );
 };
