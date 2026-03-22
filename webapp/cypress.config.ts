27829ae21c2569ee9a808db91553a62fbeb309d7
Case Walker <25988138+casewalker@users.noreply.github.com>
2026-03-22 00:20:03 -0400

    chore: Upgrade various dependencies (#5)
    Also allow various seemingly normal post-install scripts and remove a Cypress warning.

--- webapp/cypress.config.ts
+++ webapp/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  allowCypressEnv: false,
   e2e: {
     setupNodeEvents(_on: Cypress.PluginEvents, _config: Cypress.PluginConfigOptions) {
       // implement node event listeners here
